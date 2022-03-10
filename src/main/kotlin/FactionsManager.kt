2c6702222d87270482b0e6de20eb6ae65352fe2b
Marcato Luca <marcato@intre.it>
2022-03-10 09:38:53 +0100

    two characters join same Factions

--- src/main/kotlin/FactionsManager.kt
+++ src/main/kotlin/FactionsManager.kt
@@ -2,7 +2,11 @@ class FactionsManager {
     private val factions: MutableMap<String, MutableSet<RPGCharacter>> = mutableMapOf()
 
     fun addCharacterToFaction(character: RPGCharacter, faction: String) {
-        factions[faction] = mutableSetOf(character)
+        if(factions.containsKey(faction)) {
+            factions[faction]?.add(character)
+        } else {
+            factions[faction] = mutableSetOf(character)
+        }
     }
 
     fun isCharacterMemberOf(character: RPGCharacter, faction: String): Boolean {
--- src/test/kotlin/TestFactionsManager.kt
+++ src/test/kotlin/TestFactionsManager.kt
@@ -1,6 +1,4 @@
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 
 class TestFactionsManager {
@@ -12,6 +10,32 @@ class TestFactionsManager {
         factionsManager.addCharacterToFaction(character, "faction")
 
         assertTrue(factionsManager.isCharacterMemberOf(character, "faction"))
+    }
+
+    @Test
+    fun `two characters join different Factions`() {
+        val character1 = RPGCharacter()
+        val character2 = RPGCharacter()
+        val factionsManager = FactionsManager()
+
+        factionsManager.addCharacterToFaction(character1, "faction1")
+        factionsManager.addCharacterToFaction(character2, "faction2")
+
+        assertTrue(factionsManager.isCharacterMemberOf(character1, "faction1"))
+        assertTrue(factionsManager.isCharacterMemberOf(character2, "faction2"))
+    }
+
+    @Test
+    fun `two characters join same Factions`() {
+        val character1 = RPGCharacter()
+        val character2 = RPGCharacter()
+        val factionsManager = FactionsManager()
+
+        factionsManager.addCharacterToFaction(character1, "faction")
+        factionsManager.addCharacterToFaction(character2, "faction")
+
+        assertTrue(factionsManager.isCharacterMemberOf(character1, "faction"))
+        assertTrue(factionsManager.isCharacterMemberOf(character2, "faction"))
     }
 
 }
