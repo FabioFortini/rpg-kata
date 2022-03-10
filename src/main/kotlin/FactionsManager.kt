471d0bcc45391c45a8a0f283a569a69f642c36d6
Marcato Luca <marcato@intre.it>
2022-03-10 09:29:34 +0100

    implement join faction into factionManager

--- src/main/kotlin/FactionsManager.kt
+++ src/main/kotlin/FactionsManager.kt
@@ -1,9 +1,12 @@
 class FactionsManager {
-    fun addCharacterToFaction(character: RPGCharacter, s: String) {
+    private val factions: MutableMap<String, MutableSet<RPGCharacter>> = mutableMapOf()
+
+    fun addCharacterToFaction(character: RPGCharacter, faction: String) {
+        factions[faction] = mutableSetOf(character)
     }
 
-    fun isCharacterMemberOf(character: RPGCharacter, s: String): Boolean {
-        return false
+    fun isCharacterMemberOf(character: RPGCharacter, faction: String): Boolean {
+        return factions[faction]?.contains(character) ?: false
     }
 
 }
